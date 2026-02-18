edd97d5fcb6d0a2222d7d06bd146c2db827cd309
MHermosillo <143014585+MHermosillo7@users.noreply.github.com>
2026-02-17 18:46:26 -0700

    Created framework blueprints
    Uploaded basic damage system capable of receiving damage and computing effects
    
    Created basic organization for ranger attack logic
    
    TODO:
    
    - Improving camera system so its similar to horror games
    - Getting assets to create a horror feel
    - Improving movement system so it is not as trippy and follows camera facing direction

---
+++ Assets/Scripts/Movement/MouseByMovement.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+//Work in progress toward a movement system
+//similar to RE where the direction you move
+//changes depending on where the camera is facing
+public class MouseByMovement : MonoBehaviour
+{
+    Rigidbody rb;
+
+    public float speed = 2f;
+
+    public float horizontalInput = 0;
+    public float verticalInput = 0;
+
+    public float jumpForce = 3f;
+    
+    public Transform cam;
+    public Transform model;
+    public CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)
+            || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            EvaluateWASD();
+        }
+        else
+        {
+            rb.linearVelocity = Vector3.zero;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump(jumpForce);
+        }
+    }
+
+    void EvaluateWASD()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            verticalInput = 1;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            verticalInput = -1;
+        }
+        else
+        {
+            verticalInput = 0;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            horizontalInput = 1;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            horizontalInput = -1;
+        }
+        else
+        {
+            horizontalInput = 0;
+        }
+
+        Move(horizontalInput, verticalInput, speed);
+    }
+    void Move(float horizontalInput, float verticalInput, float speed)
+    {
+        Vector3 forwardDirection = cam.forward;
+
+        forwardDirection = forwardDirection * verticalInput;
+
+        Vector3 rightDirection = cam.right;
+
+        rightDirection = rightDirection * horizontalInput;
+
+        Vector3 moveDirection = forwardDirection + rightDirection;
+
+        rb.AddForce(moveDirection * speed, ForceMode.Impulse);
+
+        //Vector3 lookDirection = new Vector3(moveDirection.x, 0, moveDirection.z);
+        //model.rotation = Quaternion.LookRotation(lookDirection * speed, Vector3.up);
+    }
+
+    void Jump(float jumpForce)
+    {
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+}
